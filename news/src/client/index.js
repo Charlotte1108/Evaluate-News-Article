19de3802f74bb111afeed661cd2c90fb5e4b15e3
Charlotte Van Damme <charlotte.vdamme@hotmail.com>
2021-01-19 18:46:06 +0100

    Add files via upload

---
+++ news/src/client/index.js
@@ -0,0 +1,11 @@
+import { handleSubmit } from '.js/formHandler'
+import { functionName } from '.js/fileName'
+alert('I exist!')
+
+import './styles/resets.scss'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+
+export { handleSubmit };
